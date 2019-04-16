5d119b5dc968bbea7d5a11f63aaacf0113ad60bc
Alex Borst <butler.borst@gmail.com>
2019-04-16 23:16:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -103,8 +103,9 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     programmer_hash[:alan_kay][:known_for] = alans_new_info
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     
      
      
 end
